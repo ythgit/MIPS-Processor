8e17af2c07b09e525f6ff8389aee3e84032cd274
Yiheng Chi <chi14@purdue.edu>
2017-01-13 15:25:01 -0500

    register_file wave.do & 2nd version tb

--- testbench/register_file_tb.sv
+++ testbench/register_file_tb.sv
@@ -16,6 +16,7 @@ module register_file_tb;
   parameter PERIOD = 10;
 
   logic CLK = 0, nRST;
+  integer tb_index;
 
   // test vars
   int v1 = 1;
@@ -29,15 +30,7 @@ module register_file_tb;
   register_file_if rfif ();
   // test program
   test PROG (
-    .\rfif.rdat2 (rfif.rdat2),
-    .\rfif.rdat1 (rfif.rdat1),
-    .\rfif.wdat (rfif.wdat),
-    .\rfif.rsel2 (rfif.rsel2),
-    .\rfif.rsel1 (rfif.rsel1),
-    .\rfif.wsel (rfif.wsel),
-    .\rfif.WEN (rfif.WEN),
-    .\nRST (nRST),
-    .\CLK (CLK)
+    CLK, nRST, rfif
   );
   // DUT
 `ifndef MAPPED
@@ -56,6 +49,7 @@ module register_file_tb;
   );
 `endif
 
+  // initialize signals
   task init;
     begin
       rfif.rdat2 = 32'b0;
@@ -72,6 +66,7 @@ module register_file_tb;
     end
   endtask
 
+  // asynchronously reset
   task rst;
     begin
       @(negedge CLK);
@@ -81,6 +76,7 @@ module register_file_tb;
     end
   endtask
 
+  // write a 32-bit pack to a selected reg pos
   task write;
     input logic [31:0] wdat;
     input logic [4:0] wsel;
@@ -89,11 +85,13 @@ module register_file_tb;
       rfif.wsel = wsel;
       rfif.WEN = 1'b1;
       @(posedge CLK);
+      @(posedge CLK);
       rfif.WEN = 1'b0;
       @(posedge CLK);
     end
   endtask
 
+  // read a selected reg pos to rdat1
   task read1;
     input logic [4:0] rsel;
     begin
@@ -102,11 +100,32 @@ module register_file_tb;
     end
   endtask
 
+  // read a selected reg pos to rdat2
   task read2;
     input logic [4:0] rsel;
     begin
       rfif.rsel2 = rsel;
       #(1*PERIOD);
+    end
+  endtask
+
+  // read all reg in 32 cycles to rdat2
+  task showreg;
+    begin
+      for (tb_index = 0; tb_index < 32; tb_index = tb_index + 1) begin
+        rfif.rsel2 = tb_index;
+        #(1*PERIOD);
+      end
+    end
+  endtask
+
+  // set all reg
+  task setreg;
+    begin
+      for (tb_index = 0; tb_index < 32; tb_index = tb_index + 1) begin
+        write(32'hFFFFFFFF, tb_index);
+        #(1*PERIOD);
+      end
     end
   endtask
 
@@ -121,19 +140,38 @@ initial
 begin
   init;
 
+  // verify writes and reads to registers
   write(32'hAAAAAAAA, 5'b00001);
-  write(32'hAAAAAAAA, 5'b00010);
-  write(32'hAAAAAAAA, 5'b00011);
+  write(32'hBBBBBBBB, 5'b00010);
+  write(32'hCCCCCCCC, 5'b00011);
 
   read1(5'b00001);
   read2(5'b00011);
   read1(5'b00010);
 
+  write(32'hA0B1C2D3, 5'b01010);
+  read2(5'b01010);
+  read1(5'b00011);
+
+  write(32'hFFFF0000, 5'b11111);
+
+  // test asynchronous reset of register
+  showreg;
   rst;
+  showreg;
 
+  setreg;
+  rst;
+  showreg;
+
+  // test writes to register 0
   write(32'hFFFFFFFF, 5'b00000);
-
   read2(5'b00000);
+
+  write(32'hF07FF70F, 5'b0000);
+  read1(5'b00000);
+
+  showreg;
 
   rst;
 
