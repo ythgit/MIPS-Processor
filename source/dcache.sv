25d9029e78a309f0b1f3e2f51a6b2ea2902f0824
hythzz <anthonyhyt@gmail.com>
2017-03-07 14:01:44 -0500

    finish dcache code without testing

--- source/dcache.sv
+++ source/dcache.sv
@@ -15,4 +15,131 @@ module dcache (
   import caches_types_pkg::*;
   import cpu_types_pkg::*;
 
+  //variable declaration ---------------------------------
 
+  //counter signals
+    //counter clear
+  logic clr_ct_clr;
+    //counter enable signal
+  logic hit_ct_en, clr_ct_en;
+    //counter output
+  logic hit_ct_o_en;
+  word_t hit_num;
+  logic [3:0] clr_num;
+
+  //signal for entering final flush phase
+  logic cclear;
+
+  //variable meaning recognized by name
+  logic block_offset_cu, dhit, dhit0, dhit1, dirty0, dirty1, dirties;
+
+  //cache flip flops signals
+  word_t datatocache;
+  logic [2:0] index;
+  logic WEN, invalidate;
+  //dc_frame_t cur_frame;
+  //dc_set_t cur_set;
+  dc_block_t todp, tomem;
+  word_t cacheout, memaddr, cacheaddr;
+
+  //temp use variable for generation of dirties
+  genvar i, j;
+  logic [15:0] alldirties;
+
+  //major component instantiation and declaration --------
+  dc_set_t dcbuf [7:0];
+  logic lru [7:0];
+  flex_counter #(.BITS(32)) HITCT (
+    CLK, nRST,
+    1'b0, hit_ct_en & (dcif.dmemREN | dcif.dmemWEN), hit_num
+  );
+  flex_counter #(.BITS(4)) CLRCT (
+    CLK, nRST,
+    clr_ct_clr, clr_ct_en, clr_num
+  );
+  dcache_cu DCU (
+    CLK, nRST,
+    dirty0, dirty1, dirties, dhit, lru[index], cif.dwait, dcif.halt,
+    dcif.dmemREN, dcif.dmemWEN, clr_num,
+    cif.dREN, cif.dWEN, clr_ct_en, hit_ct_en, hit_ct_o_en,
+    cclear, dcif.flushed, block_offset_cu, clr_ct_clr, invalidate
+  );
+
+  //variable cast ----------------------------------------
+  dc_pc_t addr = dc_pc_t'(dcif.dmemaddr);
+
+  //data caches configuration ----------------------------
+    //assignment for unused signals in multicore
+  assign cif.ccwrite = 1'b0;
+  assign cif.cctrans = 1'b0;
+
+    //abbr for some signal
+  assign WEN = ~cif.dwait & (dcif.dmemREN | dcif.dmemWEN);
+  //alias  dcbuf[index] = cur_set;
+  //alias  cur_frame = cur_set[lru[index]];
+  assign dirty0 = dcbuf[index][0].dcdirty;
+  assign dirty1 = dcbuf[index][1].dcdirty;
+
+    //dirties signal generation
+  assign dirties = |alldirties;
+  generate
+    for (i = 0; i < 8; i++) begin
+      for (j = 0; j < 2; j++)
+        assign alldirties[2*i+j] = (dcbuf[i][j].dcvalid & dcbuf[i][j].dcdirty);
+    end
+  endgenerate
+
+    //dhit generation
+  assign dhit0 = (addr.dcpctag == dcbuf[index][0].dctag) & dcbuf[index][0].dcvalid;
+  assign dhit1 = (addr.dcpctag == dcbuf[index][1].dctag) & dcbuf[index][1].dcvalid;
+  assign dhit = dhit0 | dhit1;
+
+    //cache index select
+  assign index = cclear ? clr_num[3:1] : addr.dcpcind;
+
+    //cache store source select
+  assign datatocache = cif.dwait ? dcif.dmemstore : cif.dload;
+
+    //data load to datapath select
+  assign dcif.dmemload = dcbuf[index][dhit0][addr.dcpcblof];
+
+    //data store to mem select
+  assign tomem = lru[index]&~cclear | clr_num[0]&cclear ? dcbuf[index][1].dcblock : dcbuf[index][0].dcblock;
+  assign cacheout = tomem[block_offset_cu];
+  assign cif.dstore = hit_ct_o_en ? hit_num : cacheout;
+
+    //memory address select
+  assign cacheaddr = {dcbuf[index][clr_num[0]].dctag, clr_num[3:1], block_offset_cu, 2'b00};
+  assign memaddr = cclear ? cacheaddr : dcif.dmemaddr;
+  assign cif.daddr = hit_ct_o_en ? hit_num : memaddr;
+
+    //data caches flip-flops
+  always_ff @ (posedge CLK, negedge nRST)
+  begin
+    if (~nRST)
+      dcbuf <= '{default: '0};
+    else if (WEN) begin
+      dcbuf[index][lru[index]].dctag <= addr.dcpctag;
+      dcbuf[index][lru[index]].dcblock[block_offset_cu] <= datatocache;
+      if (~dcbuf[index][lru[index]].dcvalid) begin
+        dcbuf[index][lru[index]].dcvalid <= 1'b1;
+        dcbuf[index][lru[index]].dcdirty <= 1'b0;
+      end else begin
+        if (~dcbuf[index][lru[index]].dcdirty)
+          dcbuf[index][lru[index]].dcdirty <= 1'b1;
+      end
+    end
+    if (invalidate)
+      dcbuf[index][lru[index]].dcvalid <= 1'b0;
+  end
+
+    //lru flip-flops
+  always_ff @ (posedge CLK, negedge nRST)
+  begin
+    if (~nRST)
+      lru <= '{default: '0};
+    else if (WEN)
+      lru[index] <= dhit0;
+  end
+
+endmodule
--- source/dcache_cu.sv
+++ source/dcache_cu.sv
@@ -4,7 +4,7 @@ module dcache_cu (
   input logic dmemREN, dmemWEN,
   input logic [3:0] count,
   output logic dREN, dWEN, clr_ct_en, hit_ct_en, hit_ct_o_en,
-  output logic cclear, halt, block_offset
+  output logic cclear, halt, block_offset, clr_ct_clr, invalidate
 );
 
   typedef enum logic [3:0] {
@@ -16,45 +16,150 @@ module dcache_cu (
   logic dirty;
 
   assign dirty = ~lru & dirty0 | lru & dirty1;
+  assign clr_ct_clr = ~cclear;
+
+  always_comb
+  begin:output_logic
+    dREN = 1'b0;
+    dWEN = 1'b0;
+    clr_ct_en = 1'b0;
+    hit_ct_en = 1'b0;
+    hit_ct_o_en = 1'b0;
+    cclear = 1'b0;
+    halt = 1'b0;
+    block_offset = 1'b0;
+    invalidate = 1'b0;
+    casez(state)
+      //normal operation
+      IDLE: hit_ct_en = 1'b1;
+      WB1: begin
+        dWEN = 1'b1;
+        invalidate = 1'b1;
+      end
+      WB2: begin
+        dWEN = 1'b1;
+        block_offset = 1'b1;
+      end
+      READ1: dREN = 1'b1;
+      READ2: begin
+        dREN = 1'b1;
+        block_offset = 1'b1;
+      end
+      //flush and halt operation
+      COUNT: begin
+        dWEN = 1'b1;
+        cclear = 1'b1;
+        clr_ct_en = 1'b1;
+      end
+      FLUSH1: begin
+        dWEN = 1'b1;
+        cclear = 1'b1;
+        invalidate = 1'b1;
+      end
+      FLUSH2: begin
+        dWEN = 1'b1;
+        cclear = 1'b1;
+        block_offset = 1'b1;
+      end
+      CTSTORE: begin
+        dWEN = 1'b1;
+        hit_ct_o_en = 1'b1;
+      end
+      HALT: halt = 1'b1;
+    endcase
+  end
 
   always_comb
   begin:transition_logic
     casez(state)
+      //normal operation
       IDLE: begin
-        if ((dREN | dWEN) & ~dwait) begin
+        if ((dmemREN | dmemWEN) & ~dhit) begin
           if (dirty)
             nxtstate = WB1;
           else
             nxtstate = READ1;
         end else if (flush) begin
           if (dirties)
-            nxtstate = COUNT;
+            nxtstate = FLUSH1;
           else
             nxtstate = CTSTORE;
         end else
           nxtstate = state;
       end
       IDLE2: begin
-        if ((dREN | dWEN) & ~dwait) begin
+        if ((dmemREN | dmemWEN) & ~dhit) begin
           if (dirty)
             nxtstate = WB1;
           else
             nxtstate = READ1;
         end else if (flush) begin
           if (dirties)
-            nxtstate = COUNT;
+            nxtstate = FLUSH1;
           else
             nxtstate = CTSTORE;
         end else
           nxtstate = IDLE;
       end
       WB1: begin
-        if (dWEN
-
+        if (dmemWEN & ~dwait)
+          nxtstate = WB2;
+        else
+          nxtstate = state;
+      end
+      WB2: begin
+        if (dmemWEN & ~dwait)
+          nxtstate = READ1;
+        else
+          nxtstate = state;
+      end
+      READ1: begin
+        if (dmemREN & ~dwait)
+          nxtstate = READ2;
+        else
+          nxtstate = state;
+      end
+      READ2: begin
+        if (dmemREN & ~dwait)
+          nxtstate = IDLE2;
+        else
+          nxtstate = state;
+      end
+      //flush and halt operation
+      COUNT: begin
+        if (~dwait)
+          nxtstate = FLUSH2;
+        else
+          nxtstate = FLUSH1;
+      end
+      FLUSH1: begin
+        if (~dwait)
+          nxtstate = FLUSH2;
+        else
+          nxtstate = state;
+      end
+      FLUSH2: begin
+        if (~dwait) begin
+          if (~dirties | count == 4'hf)
+            nxtstate = CTSTORE;
+          else
+            nxtstate = COUNT;
+        end else
+          nxtstate = state;
+      end
+      CTSTORE: begin
+        if (~dwait)
+          nxtstate = HALT;
+        else
+          nxtstate = state;
+      end
+      HALT: nxtstate = IDLE;
+      default: nxtstate = IDLE;
+    endcase
   end
 
 
-  always_ff (posedge CLK, negedge nRST)
+  always_ff @ (posedge CLK, negedge nRST)
   begin
     if (~nRST)
       state <= IDLE;
