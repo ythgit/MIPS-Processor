bd5c9ce80b356ac3376baaeeaba9067e353a681e
ythgit <24907190+ythgit@users.noreply.github.com>
2017-04-03 01:44:06 -0400

    first draft for multicore dcache

--- source/dcache_cu.sv
+++ source/dcache_cu.sv
@@ -3,41 +3,38 @@ module dcache_cu (
   input logic dirty,
   input logic dhit, dwait,
   input logic dmemREN, dmemWEN, flush,      //signal from dp
+  input logic ccwait, ccwrite,              //signal from mem
   output logic dREN, dWEN,                  //signal to mem
   output logic flctup,                      //flush counter
   input logic [4:0] flctout,
-  output logic hitctup, hitctdn, hitctout,  //hit counter
   output logic blof, invalid,
   output logic flushing, halt
 );
 
   typedef enum logic [3:0] {
-    IDLE, WB1, WB2, MISSCT, READ1, READ2,
-    FLSTART, FLUSH1, FLUSH2, FLCT, CTSTORE, HALT
+    IDLE, WB1, WB2, WB3, WB4, READ1, READ2,
+    FLSTART, FLUSH1, FLUSH2, FLCT, HALT
   } state_t;
 
   state_t state, nxtstate;
-  logic miss;
+  logic miss, meminvalid;
 
   //some combinational logic
   assign miss = (dmemREN | dmemWEN) & ~dhit;
+  assign meminvalid = ccwait & ccwrite;
 
   always_comb
   begin:output_logic
     dREN = 1'b0;
     dWEN = 1'b0;
     flctup = 1'b0;
-    hitctup = 1'b0;
-    hitctout = 1'b0;
     flushing = 1'b0;
     halt = 1'b0;
     blof = 1'b0;
     invalid = 1'b0;
-    hitctdn = 1'b0;
     casez(state)
       //normal operation
       IDLE: begin
-        hitctup = 1'b1;
       end
       WB1: begin
         dWEN = 1'b1;
@@ -47,10 +44,13 @@ module dcache_cu (
         dWEN = 1'b1;
         blof = 1'b1;
       end
-      MISSCT: begin
-        dREN = 1'b1;
+      WB3: begin
+        dWEN = 1'b1;
         invalid = 1'b1;
-        hitctdn = 1'b1;
+      end
+      WB4: begin
+        dWEN = 1'b1;
+        blof = 1'b1;
       end
       READ1: begin
         dREN = 1'b1;
@@ -77,10 +77,6 @@ module dcache_cu (
         flushing = 1'b1;
         flctup = 1'b1;
       end
-      CTSTORE: begin
-        dWEN = 1'b1;
-        hitctout = 1'b1;
-      end
       HALT: halt = 1'b1;
     endcase
   end
@@ -89,19 +85,19 @@ module dcache_cu (
   begin:transition_logic
     casez(state)
       //normal operation
-      IDLE:    nxtstate = miss ? (dirty ? WB1 : MISSCT) :
-                        (flush ? FLSTART : state);
+      IDLE:    nxtstate = miss ? (dirty ? WB1 : READ1) :
+                        (flush ? FLSTART : (meminvalid ? WB3 : state));
       WB1:     nxtstate = ~dwait ? WB2 : state;
-      WB2:     nxtstate = ~dwait ? MISSCT : state;
-      MISSCT:  nxtstate = ~dwait ? READ2 : READ1;
+      WB2:     nxtstate = ~dwait ? READ1 : state;
+      WB3:     nxtstate = ~dwait ? WB4 : state;
+      WB4:     nxtstate = ~dwait ? IDLE : state;
       READ1:   nxtstate = ~dwait ? READ2 : state;
       READ2:   nxtstate = ~dwait ? IDLE : state;
       //flush and halt operation
-      FLSTART: nxtstate = flctout != 5'h10 ? (dirty ? FLUSH1 : FLCT) : CTSTORE;
+      FLSTART: nxtstate = flctout != 5'h10 ? (dirty ? FLUSH1 : FLCT) : HALT;
       FLUSH1:  nxtstate = ~dwait ? FLUSH2 : state;
       FLUSH2:  nxtstate = ~dwait ? FLCT : state;
       FLCT:    nxtstate = FLSTART;
-      CTSTORE: nxtstate = ~dwait ? HALT : state;
       HALT:    nxtstate = state;
       default: nxtstate = IDLE;
     endcase
